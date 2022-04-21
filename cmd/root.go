252c7c275cdc96a88ab67447b33db06abcb54d52
Przemysław Robak <przemyslaw.robak@docplanner.com>
2022-04-21 18:07:06 +0200

    fix shorthand flag

--- cmd/root.go
+++ cmd/root.go
@@ -47,7 +47,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&vaultAddr, "vault-address", "a", "https://127.0.0.1:8200", "vault address")
 	rootCmd.PersistentFlags().StringVarP(&vaultNamespace, "vault-namespace", "n", "admin", "vault namespace")
 	rootCmd.PersistentFlags().StringVarP(&vaultToken, "vault-token", "t", "", "vault token")
-	rootCmd.PersistentFlags().DurationVarP(&vaultTimeout, "vault-timeout", "timeout", 60*time.Second, "vault client timeout")
+	rootCmd.PersistentFlags().DurationVar(&vaultTimeout, "vault-timeout", 60*time.Second, "vault client timeout")
 	rootCmd.PersistentFlags().StringVar(&s3AccessKey, "s3-access-key", "", "s3 access key")
 	rootCmd.PersistentFlags().StringVar(&s3SecretKey, "s3-secret-key", "", "s3 secret key")
 	rootCmd.PersistentFlags().StringVar(&s3Bucket, "s3-bucket", "", "s3 bucket")
